392c4a28e51679bba56200fd8d2891c627906c80
Uladzislau Loputs <vloput@gmail.com>
2017-12-15 08:40:50 +0300

    Missed file. Again.

---
+++ Lab13/Lab13/LVILog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Lab13
+{
+    class LVILog
+    {
+        public static void AddNote(string utility, string path)
+        {
+            StreamWriter logFile = new StreamWriter("lvilogfile.txt", true);
+            logFile.WriteLine($"{utility} : {path} : {DateTime.Now}");
+            logFile.Close();
+        }
+    }
+}
